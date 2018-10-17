60c6d3e849a5d1ae079a6cb0ea8177fd4c683e71
Erick Reyes <erickreyes@google.com>
2018-10-17 14:06:26 -0700

    arm64: Fix build errors from vDSO update
    Change-Id: I5212100510e6f7426e9fd486fc63c0a082ecbc2b
    Signed-off-by: Erick Reyes <erickreyes@google.com>
    Signed-off-by: Miguel de Dios <migueldedios@google.com>

--- arch/arm64/kernel/vdso.c
+++ arch/arm64/kernel/vdso.c
@@ -260,7 +260,7 @@ static int vdso_setup(struct mm_struct *mm,
 
 	vdso_base = get_unmapped_area(NULL, 0, vdso_mapping_len, 0, 0);
 	if (IS_ERR_VALUE(vdso_base))
-		return PTR_ERR_OR_ZERO(ERR_PTR(vdso_base));
+		ret = ERR_PTR(vdso_base);
 
 	ret = _install_special_mapping(mm, vdso_base, PAGE_SIZE,
 				       VM_READ|VM_MAYREAD,

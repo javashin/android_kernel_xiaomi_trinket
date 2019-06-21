84bf569d44e9e4be5889b7e53d504374fc205bb0
Yaroslav Furman <yaro330@gmail.com>
2019-06-22 00:59:56 +0300

    msm-tsens: ifdef IPC logging related code
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Adithya R <radithya2002@gmail.com>

--- drivers/thermal/msm-tsens.c
+++ drivers/thermal/msm-tsens.c
@@ -310,6 +310,7 @@ int tsens_tm_probe(struct platform_device *pdev)
 		return rc;
 	}
 
+#ifdef CONFIG_IPC_LOGGING
 	snprintf(tsens_name, sizeof(tsens_name), "tsens_%pa_0",
 					&tmdev->phys_addr_tm);
 

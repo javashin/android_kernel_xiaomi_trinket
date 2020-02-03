8e2de8efb3561bcb87aef71cb73da848650b32ce
Rohit kumar <rohitkr@codeaurora.org>
2020-02-03 17:45:56 +0530

    asoc: Update supported rate and format for dummy dai
    Add support for 384KHz sample rate and S24_3LE
    bitwidth for dummy dai.
    
    Change-Id: I7d20311e58f6ec1f444a46908fd4827be32bed17
    Signed-off-by: Rohit kumar <rohitkr@codeaurora.org>
    Signed-off-by: Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>

--- sound/soc/soc-utils.c
+++ sound/soc/soc-utils.c
@@ -290,12 +290,13 @@ static const struct snd_soc_platform_driver dummy_platform = {
 
 static struct snd_soc_codec_driver dummy_codec;
 
-#define STUB_RATES	SNDRV_PCM_RATE_8000_192000
+#define STUB_RATES	SNDRV_PCM_RATE_8000_384000
 #define STUB_FORMATS	(SNDRV_PCM_FMTBIT_S8 | \
 			SNDRV_PCM_FMTBIT_U8 | \
 			SNDRV_PCM_FMTBIT_S16_LE | \
 			SNDRV_PCM_FMTBIT_U16_LE | \
 			SNDRV_PCM_FMTBIT_S24_LE | \
+			SNDRV_PCM_FMTBIT_S24_3LE | \
 			SNDRV_PCM_FMTBIT_U24_LE | \
 			SNDRV_PCM_FMTBIT_S32_LE | \
 			SNDRV_PCM_FMTBIT_U32_LE | \

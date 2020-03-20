180ea199a62a1b2129a074203892aa9e28bf35b0
Bhaumik Bhatt <bbhatt@codeaurora.org>
2020-03-19 18:27:41 -0700

    mhi: core: remove stray spinlock unlock call
    Remove stray spinlock unlock call done without a lock which
    caused a crash during a reboot.
    
    Change-Id: Iab223dee538616e4cf2c642652071d386cb13429
    Signed-off-by: Bhaumik Bhatt <bbhatt@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/bus/mhi/core/mhi_main.c
+++ drivers/bus/mhi/core/mhi_main.c
@@ -1146,11 +1146,7 @@ static void mhi_process_cmd_completion(struct mhi_controller *mhi_cntrl,
 		chan = MHI_TRE_GET_CMD_CHID(cmd_pkt);
 		if (chan >= mhi_cntrl->max_chan) {
 			MHI_ERR("invalid channel id %u\n", chan);
-<<<<<<< HEAD
-			break;
-=======
 			goto del_ring_el;
->>>>>>> 5ff5cde01cf82... mhi: core: Add range check for channel id received in event ring
 		}
 		mhi_chan = &mhi_cntrl->mhi_chan[chan];
 		write_lock_bh(&mhi_chan->lock);

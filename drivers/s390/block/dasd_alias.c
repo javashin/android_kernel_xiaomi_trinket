ca2c4546f70bb9f6557eb4a8ab516416d834b974
Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>
2021-01-09 12:46:21 -0400

    Revert "4.14 Fixes."
    This reverts commit 95301515a3044b3bc252e9d292b97bb6957ad646.

--- drivers/s390/block/dasd_alias.c
+++ drivers/s390/block/dasd_alias.c
@@ -462,19 +462,11 @@ static int read_unit_address_configuration(struct dasd_device *device,
 	spin_unlock_irqrestore(&lcu->lock, flags);
 
 	rc = dasd_sleep_on(cqr);
-	if (!rc)
-		goto out;
-
-	if (suborder_not_supported(cqr)) {
-		/* suborder not supported or device unusable for IO */
-		rc = -EOPNOTSUPP;
-	} else {
-		/* IO failed but should be retried */
+	if (rc && !suborder_not_supported(cqr)) {
 		spin_lock_irqsave(&lcu->lock, flags);
 		lcu->flags |= NEED_UAC_UPDATE;
 		spin_unlock_irqrestore(&lcu->lock, flags);
 	}
-out:
 	dasd_kfree_request(cqr, cqr->memdev);
 	return rc;
 }
